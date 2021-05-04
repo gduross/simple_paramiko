971fd2ef9b7673d2e88615712e6e997ba58272a7
root <root@server01.basement.lab>
2021-05-03 21:39:23 -0400

    first attempt to commit

---
+++ test.py
@@ -0,0 +1,41 @@
+import paramiko
+from getpass import getpass
+import time
+
+ip = input("Please enter your IP address: ")
+username = input("Please enter your username: ")
+password = getpass()
+hostname=input("Please enter the devices new name: ")
+
+remote_conn_pre=paramiko.SSHClient()
+remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+remote_conn_pre.connect(ip, port=22, username=username,  
+                        password=password,
+                        look_for_keys=False, allow_agent=False)
+
+remote_conn = remote_conn_pre.invoke_shell()
+output = remote_conn.recv(65535)
+
+remote_conn.send("conf t\n")
+time.sleep(.5)
+output = remote_conn.recv(65535)
+#print =(output)
+
+hostname1="hostname "+hostname+"\n"
+remote_conn.send(hostname1)
+time.sleep(.5)
+
+remote_conn.send("end\n")
+time.sleep(.5)
+
+remote_conn.send("wr\n")
+time.sleep(.5)
+
+remote_conn.close()
+
+
+print("#------------------------------------\nThe new device name is :"+hostname+"\n")
+
+# Notes:
+# 5/3/2021 - took from stackoverflow and updating for python3
+# 5/3/2021 - this is now a simple program to change a devices hostname
